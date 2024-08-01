92bbbee297c1b2de64a20bc2a97cad8720a24be9
Mohamed <hamosafey184@gmail.com>
2024-08-01 15:38:45 +0300

    added email validation

--- js/script.js
+++ js/script.js
@@ -35,14 +35,31 @@ const phone = document.querySelector("phone");
 const subject = document.querySelector("subject");
 const text_message = document.querySelector("text_message");
 
+function validation() {
+    var form = document.getElementById("form");
+    var email = document.getElementById("email").value;
+    var text = document.getElementById("text");
+    var pattern = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim;
+    if (email.match(pattern)) {
+        form.classList.add("valid");
+        form.classList.remove("invalid");
+        text.innerHTML = "Email Address Is Valid";
+        text.style.color = "#00ff00";
+    } else {
+        form.classList.remove("valid");
+        form.classList.add("invalid");
+        text.innerHTML = "Email Address Is Not Valid";
+        text.style.color = "#ff0000";
+    }
+}
+
 function sendEmail() {
-    
     var fullName = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var phone = document.getElementById("phone").value;
     var subject = document.getElementById("subject").value;
     var text_message = document.getElementById("text_message").value;
-    
+
     var messageBody = "Name: " + fullName + 
     "<br>Email: " + email + 
     "<br>Phone: " + phone +
